969542c39a47a667611979f0425551f9fe6c70aa
Leila Islam Ara <leilayislam@gmail.com>
2018-06-15 15:44:24 +0100

    Update pixelvaesamples.py

--- gen_models/PixelVAE/pixelvaesamples.py
+++ gen_models/PixelVAE/pixelvaesamples.py
@@ -858,7 +858,7 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as session:
 
         num = 10 # LEILAEDIT
         
-        for imnum in range(num): # LEILAEDIT
+        for imnum in xrange(num): # LEILAEDIT
             sample_fn_latents1 = np.random.normal(size=(1, LATENT_DIM_2)).astype('float32') # changed 8 to 1
             def generate_and_save_samples(tag):
                 def color_grid_vis(X, nh, nw, save_path):
