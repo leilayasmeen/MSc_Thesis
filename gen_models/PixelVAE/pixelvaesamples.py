6cbfa17163a3e933312a1b0623b00c38b07dbaf2
Leila Islam Ara <leilayislam@gmail.com>
2018-06-16 14:24:11 +0100

    Update pixelvaesamples.py

--- gen_models/PixelVAE/pixelvaesamples.py
+++ gen_models/PixelVAE/pixelvaesamples.py
@@ -10,7 +10,7 @@ sys.path.append(os.getcwd())
 N_GPUS = 2
 
 import tflib as lib
-import tflib.train_loop_4
+import tflib.sampler_loop
 import tflib.ops.kl_unit_gaussian
 import tflib.ops.kl_gaussian_gaussian
 import tflib.ops.conv2d
@@ -918,7 +918,7 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as session:
         staircase=True
     )
 
-    lib.train_loop_4.train_loop( #LEIlAEDIT: change to train_loop_4
+    lib.sampler_loop.train_loop( #LEIlAEDIT to allow sampling without training. TODO: remove unecessary arguments
         session=session,
         inputs=[total_iters, all_images],
         inject_iteration=True,
