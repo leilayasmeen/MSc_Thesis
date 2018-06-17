6ef5b9e27cb10539efdbc77992681588c53d5417
Leila Islam Ara <leilayislam@gmail.com>
2018-06-17 22:15:36 +0100

    Update pixelvaeinterpolator.py

--- gen_models/PixelVAE/pixelvaeinterpolator.py
+++ gen_models/PixelVAE/pixelvaeinterpolator.py
@@ -839,8 +839,8 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as session:
                 y_trainsubset1 = y_train[idx1,:]
                 y_trainsubset2 = y_train[idx2,:]
                 
-                imageindex1 = random.sample(range(0, x_trainsubset1.shape[0]-1))
-                imageindex2 = random.sample(range(0, x_trainsubset2.shape[0]-1))
+                imageindex1 = random.sample(range(0, x_trainsubset1.shape[0]-1),1)
+                imageindex2 = random.sample(range(0, x_trainsubset2.shape[0]-1),1)
                 
                 # Draw the corresponding images and labels from the training data
                 image1 = x_trainsubset1[imageindex1,:]
