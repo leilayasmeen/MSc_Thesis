58b81bb19c915809977d4669ceabc3fb67aecb29
Leila Islam Ara <leilayislam@gmail.com>
2018-06-20 14:59:56 +0100

    Create cifar10.py

---
+++ gen_models/PixelVAE/tflib/cifar10.py
@@ -0,0 +1,53 @@
+import sys
+sys.modules['theano'] = None
+
+import numpy as np
+
+from fuel.datasets.hdf5 import H5PYDataset
+from fuel.schemes import ShuffledScheme, SequentialScheme
+from fuel.streams import DataStream
+# from fuel.transformers.image import RandomFixedSizeCrop
+
+PATH = 'cifar10.hdf5'
+
+from scipy.misc import imsave
+def color_grid_vis(X, nh, nw, save_path):
+    # from github.com/Newmu
+    X = X.transpose(0,2,3,1)
+    h, w = X[0].shape[:2]
+    img = np.zeros((h*nh, w*nw, 3))
+    for n, x in enumerate(X):
+        j = n/nw
+        i = n%nw
+        img[j*h:j*h+h, i*w:i*w+w, :] = x
+    imsave(save_path, img)
+
+
+def _make_stream(stream, bs):
+    def new_stream():
+      result = np.empty((bs, 32, 32, 3), dtype='int32')
+      for (imb,) in stream.get_epoch_iterator():
+        for i, img in enumerate(imb):
+          result[i] =  img[:32, :32, :]                
+        yield (result.transpose(0,3,1,2),)
+    return new_stream
+
+def load(batch_size=128):
+    tr_data = H5PYDataset(PATH, which_sets=('train',))
+    te_data = H5PYDataset(PATH, which_sets=('valid',))
+
+    ntrain = tr_data.num_examples
+    nval = te_data.num_examples
+
+    print "ntrain {}, nval {}".format(ntrain, nval)
+
+    tr_scheme = ShuffledScheme(examples=ntrain, batch_size=batch_size)
+    tr_stream = DataStream(tr_data, iteration_scheme=tr_scheme)
+
+    # te_scheme = SequentialScheme(examples=ntest, batch_size=batch_size)
+    # te_stream = DataStream(te_data, iteration_scheme=te_scheme)
+
+    val_scheme = SequentialScheme(examples=nval, batch_size=batch_size)
+    val_stream = DataStream(tr_data, iteration_scheme=val_scheme)
+
+    return _make_stream(tr_stream, batch_size), _make_stream(val_stream, batch_size)
