33929d67a90273b9808f0df4bef189829c7c4718
Leila Islam Ara <leilayislam@gmail.com>
2018-07-02 19:00:02 +0100

    Update pixelvaereconstruction_cifar_filter_3.py

--- gen_models/PixelVAE/pixelvaereconstruction_cifar_filter_3.py
+++ gen_models/PixelVAE/pixelvaereconstruction_cifar_filter_3.py
@@ -961,7 +961,7 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as session:
             for imagenum in range(num):
 
                 print "Sampling Random Image Index"
-                idx = random.sample(range(0, x_train_set.shape[0]-1), 1)
+                idx = random.sample(range(0, x_train_set.shape[0]), 1)
                            
                 print "Drawing Corresponding Image and Label Out"            
                 x_train_set_array = np.array(x_train_set)
