895568c926e6273b9ba27b012df7a2bae924807e
Leila Islam Ara <leilayislam@gmail.com>
2018-07-03 21:57:10 +0100

    Update train_cifar10_leilaclip_aug.py

--- cnn_architectures/augmentation_1/resnet_cifar_aug1/train_cifar10_leilaclip_aug.py
+++ cnn_architectures/augmentation_1/resnet_cifar_aug1/train_cifar10_leilaclip_aug.py
@@ -115,12 +115,18 @@ if __name__ == '__main__':
     y_train = keras.utils.to_categorical(y_train, num_classes)
     y_test = keras.utils.to_categorical(y_test, num_classes) # test set retains one-hot encoding
     
+    # split
     x_train45, x_val, y_train45, y_val = train_test_split(x_train, y_train, test_size=0.1, random_state=seed)  # random_state = seed
     
-    x_train45 = np.load('x_augmentation_array.npy')
-    x_train45 = x_train45.transpose(0,2,3,1)
-    y_train45 = np.load('y_augmentation_array.npy')
-    y_train45 = y_train45.reshape(-1,num_classes)
+    # load augmentation sets
+    x_train45_additions = np.load('x_augmentation_array.npy')
+    x_train45_additions = x_train45_additions.transpose(0,2,3,1)
+    y_train45_additions = np.load('y_augmentation_array.npy')
+    y_train45_additions = y_train45_additions.reshape(-1,num_classes)
+    
+    # concatenate with initial training set
+    x_train45 = np.concatenate((x_train45, x_train45_additions),axis=0)
+    y_train45 = np.concatenate((y_train45, y_train45_additions), axis=0)
     
     # Normalize data with per-pixel mean
     img_mean = x_train45.mean(axis=0)  # per-pixel mean
