7c9ffb52bc9a2a78486a9531c1e4e4f35ae805d6
Leila Islam Ara <leilayislam@gmail.com>
2018-07-03 18:59:46 +0100

    Update train_cifar10_leilaclip_aug.py

--- cnn_architectures/augmentation_1/resnet_cifar_aug1/train_cifar10_leilaclip_aug.py
+++ cnn_architectures/augmentation_1/resnet_cifar_aug1/train_cifar10_leilaclip_aug.py
@@ -98,12 +98,13 @@ if __name__ == '__main__':
 
     # load data
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-    x_train = np.load('x_augmentation_array.npy')
-    x_train = x_train.transpose(0,2,3,1)
-    y_train = np.load('y_augmentation_array.npy')
     y_test = keras.utils.to_categorical(y_test, num_classes) # test set retains one-hot encoding
     
     x_train45, x_val, y_train45, y_val = train_test_split(x_train, y_train, test_size=0.1, random_state=seed)  # random_state = seed
+    
+    x_train45 = np.load('x_augmentation_array.npy')
+    x_train45 = x_train45.transpose(0,2,3,1)
+    y_train45 = np.load('y_augmentation_array.npy')
     
     # Normalize data with per-pixel mean
     img_mean = x_train45.mean(axis=0)  # per-pixel mean
