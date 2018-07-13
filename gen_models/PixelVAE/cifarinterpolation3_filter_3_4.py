536210dfa7ca537bd8cc00f7dc820e211e93fe0e
Leila Islam Ara <leilayislam@gmail.com>
2018-07-13 13:09:40 -0400

    Update cifarinterpolation3_filter_3_4.py

--- gen_models/PixelVAE/cifarinterpolation3_filter_3_4.py
+++ gen_models/PixelVAE/cifarinterpolation3_filter_3_4.py
@@ -960,7 +960,7 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as session:
                     img[j*h:j*h+h, i*w:i*w+w, :] = x
                 imsave(OUT_DIR + '/' + save_path, img)
                 
-            numsamples = 6
+            numsamples = 8
             pvals = np.linspace(0.2, 0.8, num=4)
             #pvals = np.linspace(0.2, 0.8, num=1)
             p_set = np.zeros(1)
