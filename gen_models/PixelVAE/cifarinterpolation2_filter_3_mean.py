fa6c1f3b483e1958297a7fddcc36336be6191c13
Leila Islam Ara <leilayislam@gmail.com>
2018-07-13 15:14:13 -0400

    Update cifarinterpolation2_filter_3_mean.py

--- gen_models/PixelVAE/cifarinterpolation2_filter_3_mean.py
+++ gen_models/PixelVAE/cifarinterpolation2_filter_3_mean.py
@@ -1004,7 +1004,7 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as session:
                   a = np.delete(a, -1, axis=1)
                   b = np.delete(b, -1, axis=1)
                   meandist[m] = np.linalg.norm(a-b)
-            print "distances are " meandist
+            print meandist
             
             # Sort distances between pairs and find the five smallest
             sorteddistances = np.sort(meandist)
