b8eb7b1091b6f92fafb8be3ac5788b9ea90688e8
Leila Islam Ara <leilayislam@gmail.com>
2018-07-19 20:17:59 -0400

    Update mixupcifarexamples.py

--- gen_models/PixelVAE/mixupcifarexamples.py
+++ gen_models/PixelVAE/mixupcifarexamples.py
@@ -7,6 +7,7 @@ import numpy as np
 import tensorflow as tf
 import imageio
 from imageio import imsave
+import random
 
 import keras
 
@@ -27,7 +28,6 @@ x_test_set = x_test_set.transpose(0,3,1,2)
 seed = 333
 x_train_set, x_dev_set, y_train_set, y_dev_set = train_test_split(x_train_set, y_train_set, test_size=0.1, random_state=seed)
 
-
 from keras.utils import np_utils           
             
 # Function to translate numeric images into plots
@@ -37,60 +37,44 @@ def color_grid_vis(X, nh, nw, save_path):
   h, w = X[0].shape[:2]
   img = np.zeros((h*nh, w*nw, 3))
   for n, x in enumerate(X):
-                    j = n/nw
-                    i = n%nw
-                    img[j*h:j*h+h, i*w:i*w+w, :] = x
-                imsave(OUT_DIR + '/' + save_path, img)
+      j = n/nw
+      i = n%nw
+      mg[j*h:j*h+h, i*w:i*w+w, :] = x
+  imsave(OUT_DIR + '/' + save_path, img)
                 
-            numsamples = 5
+numsamples = 1
+pvals = np.linspace(0.0, 1.0, num=6)
                 
-            for imagenum in range(numsamples):
-
-                print "Sampling Random Image Index from Each Class"
-                for k in range(NUM_CLASSES):
-                  idk = np.asarray(np.where(np.equal(y_train_set,k))[0])
-                  x_classk = np.asarray(x_train_set[idk,:])
-                  #idx = random.sample(range(0, x_classk.shape[0]), 1)
-                  idx = imagenum
+print "Sampling First Five Image Indices from Each Class"
+for k in range(NUM_CLASSES-1):
+      idk = np.asarray(np.where(np.equal(y_train_set,k))[0])
+      x_classk = np.asarray(x_train_set[idk,:])
+      idx = random.sample(range(0, x_classk.shape[0]), 1)
                            
-                  print "Drawing Corresponding Image and Label Out"            
-                  #x_train_set_array = np.array(x_train_set)
-                  #image = x_train_set_array[idx,:]
-                  x_classk_array = np.array(x_classk)
-                  image = x_classk[idx,:]
+      print "Drawing Corresponding Image and Label Out"            
+      x_classk_array = np.array(x_classk)
+      image1 = x_classk[idx,:]
 
-                  # Reshape
-                  image = image.reshape(1, N_CHANNELS, HEIGHT, WIDTH)
-                  
-                  # Encode the images
-                  image_code = enc_fn(image)
-               
-                  samples = np.zeros(
-                     (1, N_CHANNELS, HEIGHT, WIDTH), 
-                     dtype='int32'
-                  )
+      # Reshape
+      image1 = image1.reshape(1, N_CHANNELS, HEIGHT, WIDTH)
+      
+      for j in range(k+1,NUM_CLASSES):
+            idj = np.asarray(np.where(np.equal(y_train_set,j))[0])
+            x_classj = np.asarray(x_train_set[idj,:])
+            idx2 = random.sample(range(0, x_classj.shape[0]), 1)
+                           
+            print "Drawing Corresponding Image and Label Out"            
+            image2 = x_classj[idx2,:]
 
-                  print "Generating samples"
-                  for y in xrange(HEIGHT):
-                     for x in xrange(WIDTH):
-                           for ch in xrange(N_CHANNELS):
-                              next_sample = dec1_fn(image_code, samples, ch, y, x) 
-                              samples[:,ch,y,x] = next_sample
-                            
-                  #LEILAEDIT for .npy saving
-                  x_augmentation_set = np.concatenate((x_augmentation_set, samples), axis=0)#LEILAEDIT for .npy saving
-                
-                  print "Saving original sample"
+            # Reshape
+            image2 = image2.reshape(1, N_CHANNELS, HEIGHT, WIDTH)
+            print "Saving mixed examples"
+            
+            for p in pvals:
+                  sample = p*image1 + (1.0-p)*image2
                   color_grid_vis(
-                     image, 
-                     1, 
-                     1, 
-                     'original_class{}_{}.png'.format(k,imagenum)
-                  )
-                  print "Saving reconstructed sample"
-                  color_grid_vis(
-                     samples, 
-                     1, 
-                     1, 
-                     'reconstruction_filter_5_class{}_{}.png'.format(k,imagenum)
+                        sample,
+                        1,
+                        1,
+                        'mixup_class{}_andclass{}_pvaluefirstclass{}.png'.format(k,j,p)
                   )
