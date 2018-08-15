95494f68a4a09ef3c074b1851a8f6a10633f69bc
Leila Islam Ara <leilayislam@gmail.com>
2018-08-15 21:58:40 +0100

    Update train_model_fixed_augmentations.py

--- final_code/nn_training_scripts/train_model_fixed_augmentations.py
+++ final_code/nn_training_scripts/train_model_fixed_augmentations.py
@@ -1,6 +1,8 @@
-# This file trains a ResNet-110, with a user-specified data augmentation set that is added to the original training set.
-# It is based off a script implemented by Markus Kangsepp
-# ResNet model from https://github.com/BIGBALLON/cifar-10-cnn/blob/master/4_Residual_Network/ResNet_keras.py
+# This file trains a ResNet-110, with a user-specified data
+# augmentation set that is added to the original training set.
+# It is based off a script implemented by Markus Kangsepp.
+# The ResNet model is from:
+# https://github.com/BIGBALLON/cifar-10-cnn/blob/master/4_Residual_Network/ResNet_keras.py
 
 import keras
 import numpy as np
@@ -21,7 +23,8 @@ img_rows, img_cols = 32, 32
 img_channels       = 3
 batch_size         = 128
 epochs             = 200
-iterations         = 49500 // batch_size # Edit the numerator to equal the total augmented training set size (45,000 + aug_set_size)
+# Edit the numerator to equal the total training set size (45,000 + aug_set_size)
+iterations         = 49500 // batch_size #
 weight_decay       = 0.0001
 
 seed = 333
@@ -46,12 +49,14 @@ def residual_network(img_input,classes_num=10,stack_n=5):
         pre_bn   = BatchNormalization()(intput)
         pre_relu = Activation('relu')(pre_bn)
 
-        conv_1 = Conv2D(out_channel,kernel_size=(3,3),strides=stride,padding='same',
+        conv_1 = Conv2D(out_channel,kernel_size=(3,3),
+                        strides=stride,padding='same',
                         kernel_initializer="he_normal",
                         kernel_regularizer=regularizers.l2(weight_decay))(pre_relu)
         bn_1   = BatchNormalization()(conv_1)
         relu1  = Activation('relu')(bn_1)
-        conv_2 = Conv2D(out_channel,kernel_size=(3,3),strides=(1,1),padding='same',
+        conv_2 = Conv2D(out_channel,kernel_size=(3,3),
+                        strides=(1,1),padding='same',
                         kernel_initializer="he_normal",
                         kernel_regularizer=regularizers.l2(weight_decay))(relu1)
         if increase:
@@ -70,7 +75,8 @@ def residual_network(img_input,classes_num=10,stack_n=5):
     # stack_n = 5 by default, total layers = 32
     # Input dimensions: 32x32x3 
     # Output dimensions: 32x32x16
-    x = Conv2D(filters=16,kernel_size=(3,3),strides=(1,1),padding='same',
+    x = Conv2D(filters=16,kernel_size=(3,3),
+               strides=(1,1),padding='same',
                kernel_initializer="he_normal",
                kernel_regularizer=regularizers.l2(weight_decay))(img_input)
 
@@ -106,13 +112,17 @@ if __name__ == '__main__':
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
     y_test = keras.utils.to_categorical(y_test, num_classes10)
     
-    # Load in the augmentation set for the experiment being run. This file should be adjusted for each distinct experiment.
-    x_train_additions = np.load('Augmentation_Sets/x_augmentation_array_mean_beta_largesample_4500.npy')
-    y_train_additions = np.load('Augmentation_Sets/y_augmentation_array_mean_beta_largesample_4500.npy')
+    # Load in the augmentation set for the experiment being run.
+    # The filename should be adjusted for each experiment.
+    x_train_additions = np.load('x_augmentation_array.npy')
+    y_train_additions = np.load('y_augmentation_array.npy')
 
-    # Split the CIFAR-10 dataset into training, validation, and test sets before appending the augmentation set
+    # Split the CIFAR-10 dataset into training, validation, and test sets
     # The random seed ensures that every discriminator uses the same split
-    x_train45, x_val, y_train45, y_val = train_test_split(x_train, y_train, test_size=0.1, random_state=seed) 
+    x_train45, x_val, y_train45, y_val = train_test_split(x_train, 
+                                                          y_train, 
+                                                          test_size=0.1, 
+                                                          random_state=seed) 
     
     # Add the augmentation set to the training set
     x_train_additions = x_train_additions.transpose(0,2,3,1)
@@ -134,14 +144,17 @@ if __name__ == '__main__':
     resnet    = Model(img_input, output)
     print(resnet.summary())
 
-    # Set the optimizer and momentum, specify gradient clipping value with the clipnorm option to ensure stability
-    sgd = optimizers.SGD(lr=.1, momentum=0.9, nesterov=True, clipnorm=1.)
-    resnet.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    # Set the optimizer and momentum, specify gradient clipping value
+    # with the clipnorm option to ensure stability
+    sgd = optimizers.SGD(lr=.1, momentum=0.9, nesterov=True, 
+                         clipnorm=1.)
+    resnet.compile(loss='categorical_crossentropy', 
+                   optimizer=sgd, metrics=['accuracy'])
 
     # Set the callback
     cbks = [LearningRateScheduler(scheduler)]
 
-    # Simple data augmentation (e.g., flips) - these do not change the class vectors
+    # Simple data augmentation - these do not change the class vectors
     print('Using real-time data augmentation.')
     datagen = ImageDataGenerator(horizontal_flip=True,
                                  width_shift_range=0.125,
@@ -150,19 +163,21 @@ if __name__ == '__main__':
     datagen.fit(x_train45)
 
     # Commence training
-    hist = resnet.fit_generator(datagen.flow(x_train45, y_train45,batch_size=batch_size),
+    hist = resnet.fit_generator(datagen.flow(x_train45,
+                                             y_train45,
+                                             batch_size=batch_size),
                          steps_per_epoch=iterations,
                          epochs=epochs,
                          callbacks=cbks,
                          validation_data=(x_val, y_val))
     
     # Save model weights once training finishes
-    resnet.save('resnet_110_45kclip_augmean_beta_largesample_4500.h5')
+    resnet.save('resnet_110_45kclip_aug.h5')
     
     print("Get test accuracy:")
     loss, accuracy = resnet.evaluate(x_test, y_test, verbose=0)
     print("Test: accuracy1 = %f  ;  loss1 = %f" % (accuracy, loss))
     
     # Save model history
-    with open('hist_110_cifar10_v2_45kclip_augmean_beta_largesample_4500.p', 'wb') as f:
+    with open('hist_110_cifar10_v2_45kclip_aug.p', 'wb') as f:
         pickle.dump(hist.history, f)
